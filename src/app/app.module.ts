5fd3d8cb36ca15f79cdd3ace4e0a2daedbf240a0
Santiago Encarnacion <Santiago.503.es@gmail.com>
2022-04-16 21:53:13 -0400

    structure of folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material/material.module';
 import { ComponentsModule } from './components/components.module';
+import { ContactComponent } from './page/contact/contact.component';
+import { FormControlContactComponent } from './page/contact/form-control-contact/form-control-contact.component';
+import { IndexContactComponent } from './page/index-contact/index-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactComponent,
+    FormControlContactComponent,
+    IndexContactComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page/contact/form-control-contact/form-control-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control-contact',
+  templateUrl: './form-control-contact.component.html',
+  styleUrls: ['./form-control-contact.component.css']
+})
+export class FormControlContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
